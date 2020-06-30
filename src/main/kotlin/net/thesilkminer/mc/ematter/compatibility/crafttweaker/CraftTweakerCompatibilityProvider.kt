01835da938dadd44a1384d3fce1197f6212cd625
TheSilkMiner <thesilkminer@outlook.com>
2020-06-30 11:22:10 +0200

    Add skeleton for CrT compatibility
    Signed-off-by: TheSilkMiner <thesilkminer@outlook.com>

---
+++ src/main/kotlin/net/thesilkminer/mc/ematter/compatibility/crafttweaker/CraftTweakerCompatibilityProvider.kt
@@ -0,0 +1,7 @@
+package net.thesilkminer.mc.ematter.compatibility.crafttweaker
+
+import net.thesilkminer.mc.boson.api.modid.CRAFT_TWEAKER_2
+import net.thesilkminer.mc.boson.prefab.compatibility.ModCompatibilityProvider
+import net.thesilkminer.mc.ematter.compatibility.EnergyIsMatterCompatibilityProvider
+
+internal class CraftTweakerCompatibilityProvider : ModCompatibilityProvider(CRAFT_TWEAKER_2), EnergyIsMatterCompatibilityProvider
