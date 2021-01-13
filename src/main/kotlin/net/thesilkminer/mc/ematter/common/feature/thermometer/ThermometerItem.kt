fc570efa7a93b9470096be505706809d3d409334
TheSilkMiner <thesilkminer@outlook.com>
2021-01-13 22:01:02 +0100

    Add first rendition of thermometer
    Missing: textures, overlay for temperature
    
    Signed-off-by: TheSilkMiner <thesilkminer@outlook.com>

---
+++ src/main/kotlin/net/thesilkminer/mc/ematter/common/feature/thermometer/ThermometerItem.kt
@@ -0,0 +1,34 @@
+package net.thesilkminer.mc.ematter.common.feature.thermometer
+
+import net.minecraft.entity.player.EntityPlayer
+import net.minecraft.init.Blocks
+import net.minecraft.item.Item
+import net.minecraft.item.ItemStack
+import net.minecraft.util.ActionResult
+import net.minecraft.util.EnumActionResult
+import net.minecraft.util.EnumHand
+import net.minecraft.util.math.BlockPos
+import net.minecraft.util.math.RayTraceResult
+import net.minecraft.util.text.TextComponentString
+import net.minecraft.world.World
+import net.thesilkminer.mc.boson.api.direction.Direction
+import net.thesilkminer.mc.boson.prefab.direction.toFacing
+import net.thesilkminer.mc.ematter.common.temperature.TemperatureContext
+import net.thesilkminer.mc.ematter.common.temperature.TemperatureTables
+
+internal class ThermometerItem : Item() {
+    override fun onItemRightClick(worldIn: World, playerIn: EntityPlayer, handIn: EnumHand): ActionResult<ItemStack> {
+        if (worldIn.isRemote) return super.onItemRightClick(worldIn, playerIn, handIn)
+
+        val rayTraceResult = this.rayTrace(worldIn, playerIn, true)
+                ?: RayTraceResult(RayTraceResult.Type.MISS, playerIn.positionVector, Direction.UP.toFacing(), BlockPos(playerIn.positionVector))
+
+        val targetState = if (rayTraceResult.typeOfHit == RayTraceResult.Type.MISS) Blocks.AIR else worldIn.getBlockState(rayTraceResult.blockPos).block
+        val currentTemperature = TemperatureTables[targetState](TemperatureContext(worldIn, rayTraceResult.blockPos, TemperatureContext.DayMoment[worldIn.worldTime]))
+
+        // TODO("Temporary, just to make this shit work")
+        playerIn.sendStatusMessage(TextComponentString(currentTemperature.toString()), true)
+
+        return ActionResult.newResult(EnumActionResult.SUCCESS, playerIn.getHeldItem(handIn))
+    }
+}
