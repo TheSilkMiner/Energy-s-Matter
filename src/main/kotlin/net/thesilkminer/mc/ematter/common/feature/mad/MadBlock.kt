6578023e4f35fe5e62655352312258bb7d16f37e
TheSilkMiner <thesilkminer@outlook.com>
2021-01-27 21:56:41 +0100

    Fancy MAD be fancy
    Signed-off-by: TheSilkMiner <thesilkminer@outlook.com>

--- src/main/kotlin/net/thesilkminer/mc/ematter/common/feature/mad/MadBlock.kt
+++ src/main/kotlin/net/thesilkminer/mc/ematter/common/feature/mad/MadBlock.kt
@@ -33,6 +33,7 @@ import net.minecraft.block.properties.PropertyEnum
 import net.minecraft.block.state.BlockStateContainer
 import net.minecraft.block.state.IBlockState
 import net.minecraft.creativetab.CreativeTabs
+import net.minecraft.entity.Entity
 import net.minecraft.entity.player.EntityPlayer
 import net.minecraft.inventory.InventoryHelper
 import net.minecraft.item.ItemStack
@@ -43,6 +44,8 @@ import net.minecraft.util.EnumHand
 import net.minecraft.util.NonNullList
 import net.minecraft.util.math.AxisAlignedBB
 import net.minecraft.util.math.BlockPos
+import net.minecraft.util.math.RayTraceResult
+import net.minecraft.util.math.Vec3d
 import net.minecraft.world.IBlockAccess
 import net.minecraft.world.World
 import net.thesilkminer.mc.ematter.EnergyIsMatter
@@ -52,6 +55,42 @@ internal class MadBlock : Block(Material.IRON) {
     internal companion object {
         internal val TIER = PropertyEnum.create("tier", MadTier::class.java)
         private val BOUNDING_BOX = AxisAlignedBB(0.0, 0.0, 0.0, 1.0, 0.6875, 1.0)
+        private val EMPTY_VOLUME = box(0, 0, 0, 0, 0, 0)
+
+        internal val volumes = sequenceOf(
+                box(1, 10, 1, 3, 11, 3), // nw
+                box(4, 10, 1, 6, 11, 3), // nnw
+                box(7, 10, 1, 9, 11, 3), // n
+                box(10, 10, 1, 12, 11, 3), // nne
+                box(13, 10, 1, 15, 11, 3), // ne
+                box(13, 10, 4, 15, 11, 6), // nee
+                box(13, 10, 7, 15, 11, 9), // e
+                box(13, 10, 10, 15, 11, 12), // see
+                box(13, 10, 13, 15, 11, 15), // se
+                box(10, 10, 13, 12, 11, 15), // sse
+                box(7, 10, 13, 9, 11, 15), // s
+                box(4, 10, 13, 6, 11, 15), // ssw
+                box(1, 10, 13, 3, 11, 15), // sw
+                box(1, 10, 10, 3, 11, 12), // sww
+                box(1, 10, 7, 3, 11, 9), // w
+                box(1, 10, 4, 3, 11, 6), // nww
+                box(4, 8, 4, 6, 9, 6), // nw
+                box(7, 8, 4, 9, 9, 6), // n
+                box(10, 8, 4, 12, 9, 6), // ne
+                box(10, 8, 7, 12, 9, 9), // e
+                box(10, 8, 10, 12, 9, 12), // se
+                box(7, 8, 10, 9, 9, 12), // s
+                box(4, 8, 10, 6, 9, 12), // sw
+                box(4, 8, 7, 6, 9, 9), // w
+                box(7, 6, 7, 9, 7, 9), // center
+                box(1, 4, 1, 15, 5, 15), // ring
+                box(0, 0, 0, 16, 4, 16), // base
+                box(2, 4, 2, 14, 6, 14) // lithium_ion_battery_pack
+        )
+
+        private fun box(fromX: Int, fromY: Int, fromZ: Int, toX: Int, toY: Int, toZ: Int) =
+                AxisAlignedBB(fromX.toDouble() / 16.0 , fromY.toDouble() / 16.0, fromZ.toDouble() / 16.0,
+                        toX.toDouble() / 16.0, toY.toDouble() / 16.0, toZ.toDouble() / 16.0)
     }
 
     init {
@@ -79,6 +118,14 @@ internal class MadBlock : Block(Material.IRON) {
         return super.breakBlock(worldIn, pos, state)
     }
 
+    override fun addCollisionBoxToList(state: IBlockState, worldIn: World, pos: BlockPos, entityBox: AxisAlignedBB,
+                                       collidingBoxes: MutableList<AxisAlignedBB>, entityIn: Entity?, isActualState: Boolean) {
+        collidingBoxes.addAll(volumes.map { it.offset(pos) }.filter { entityBox.intersects(it) })
+    }
+
+    override fun collisionRayTrace(blockState: IBlockState, worldIn: World, pos: BlockPos, start: Vec3d, end: Vec3d): RayTraceResult? =
+            volumes.map { this.rayTrace(pos, start, end, it) }.firstOrNull { it != null }
+
     override fun hasTileEntity(state: IBlockState) = true
     override fun createTileEntity(world: World, state: IBlockState): TileEntity? = MadTileEntity()
     override fun createBlockState() = BlockStateContainer(this, TIER)
@@ -89,5 +136,5 @@ internal class MadBlock : Block(Material.IRON) {
     override fun isOpaqueCube(state: IBlockState) = false
     override fun isFullCube(state: IBlockState) = false
     override fun getRenderLayer() = BlockRenderLayer.CUTOUT
-    override fun getBoundingBox(state: IBlockState, source: IBlockAccess, pos: BlockPos) = BOUNDING_BOX
+    override fun getBoundingBox(state: IBlockState, source: IBlockAccess, pos: BlockPos) = EMPTY_VOLUME
 }
